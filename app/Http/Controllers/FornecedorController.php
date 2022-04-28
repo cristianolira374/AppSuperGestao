78ea4c1e5df4b2715f23b31c4243aa71fb4998f2
cristianolira374 <csl.lira@gmail.com>
2022-04-28 13:24:26 -0300

    commit do dia 28.04

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');
+        //$fornecedores = ['fornecedor1','fornecedor1','fornecedor1','fornecedor1','fornecedor1','fornecedor1','fornecedor1','fornecedor1','fornecedor1','fornecedor1','fornecedor1'];
+        
+        //array muntidimensional
+        $fornecedores = [0 => ['nome' => 'Fornecedor1', 'status' => 'N']];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,10 +1,32 @@
 <h3>Fornecedor</h3>
 
-{{-- Comentário que será descartado pelo interpretador do Blade --}}
-
 @php
-//Comentar linhas
-/* 
-Comentar bloco
+/*    
+
 */
 @endphp
+
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@if($fornecedores[0]['status'] == 'N')
+    <h3> Fornecedor inativo </h3>
+@else
+    <h3> Fornecedor ativo </h3>
+@endif
+<br>
+
+
+@unless($fornecedores[0]['status'] == 'S')
+<h3> Fornecedor inativo </h3>
+@endunless
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
